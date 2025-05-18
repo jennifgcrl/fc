d206be9586eeff1d7d2609ce7dce6692f3a5882b
Jennifer Zhou <jennifer@jezh.me>
2025-05-18 06:06:00 +0000

    wip

--- hosts/server1/default.nix
+++ hosts/server1/default.nix
@@ -5,56 +5,40 @@
   lib,
   ...
 }: {
+  # nix
   nix.settings.experimental-features = ["nix-command" "flakes"];
+  nixpkgs.config.allowUnfree = true;
 
+  # hardware
   imports = [
     ./hardware.nix
   ];
+  services.fwupd.enable = true;
 
-  boot.loader.efi.canTouchEfiVariables = true;
+  # system
+  networking.hostName = hostName;
 
+  boot.loader.efi.canTouchEfiVariables = true; # not sure if needed
   boot.initrd.systemd.enable = true;
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
-    pkiBundle = "/var/lib/sbctl";
+    pkiBundle = "/var/lib/sbctl"; # generate with sbctl create-keys
   };
-
-  networking.hostName = hostName;
-
   time.timeZone = "UTC";
-
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     keyMap = "dvorak";
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  users.users.jennifer = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-    packages = with pkgs; [
-      tree
-      neovim
-    ];
-    shell = pkgs.zsh;
   };
 
   environment.systemPackages = with pkgs; [
     neovim
     tpm2-tss
     sbctl
+    ghostty.terminfo
   ];
 
-  environment.shells = [pkgs.zsh];
-  programs.zsh.enable = true;
-
-  services.tailscale.enable = true;
-  services.fwupd.enable = true;
-
   services.xserver.videoDrivers = ["nvidia"];
-
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
@@ -62,6 +46,18 @@
     nvidiaSettings = false;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
+
+  # user
+  users.users.jennifer = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+    shell = pkgs.zsh;
+  };
+  environment.shells = [pkgs.zsh];
+  programs.zsh.enable = true;
+
+  services.tailscale.enable = true;
+  services.eternal-terminal.enable = true;
 
   # see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
   system.stateVersion = "24.11";
