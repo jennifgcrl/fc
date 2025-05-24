6c5a28c5de62307cdfdf37178f1f918b416c689e
Jennifer Zhou <jennifer@jezh.me>
2025-05-24 22:46:05 +0000

    wip

--- profiles/nixos/nvidia.nix
+++ profiles/nixos/nvidia.nix
@@ -18,5 +18,27 @@
   services.ollama.package = pkgs.ollama-cuda;
   environment.systemPackages = with pkgs; [
     nvtopPackages.nvidia
+
+    # list may not be complete
+    cudaPackages.cuda_cuobjdump
+    cudaPackages.cuda_gdb
+    cudaPackages.cuda_nvcc
+    cudaPackages.cuda_nvdisasm
+    cudaPackages.cuda_nvprune
+    cudaPackages.cuda_cccl
+    cudaPackages.cuda_cudart
+    cudaPackages.cuda_cupti
+    cudaPackages.cuda_cuxxfilt
+    cudaPackages.cuda_nvml_dev
+    cudaPackages.cuda_nvrtc
+    cudaPackages.cuda_nvtx
+    cudaPackages.cuda_profiler_api
+    cudaPackages.cuda_sanitizer_api
+    cudaPackages.libcublas
+    cudaPackages.libcufft
+    cudaPackages.libcurand
+    cudaPackages.libcusolver
+    cudaPackages.libcusparse
+    cudaPackages.libnpp
   ];
 }
