66b64bcfeea6318f3e835240cdbffd13b63ffb38
Jennifer Zhou <jennifer@jezh.me>
2025-06-04 07:26:06 +0000

    wip

--- profiles/nixos/graphical.nix
+++ profiles/nixos/graphical.nix
@@ -105,6 +105,9 @@
         shellAliases = {
           zed = "zeditor";
         };
+        sessionVariables = {
+          XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
+        };
       };
       ghostty = {
         enable = true;
